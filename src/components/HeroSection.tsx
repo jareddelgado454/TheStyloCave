754bb5ffa1c7fef53508d51f9d97d5cccd004b5e
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2025-09-12 10:40:55 -0500

    last changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { LuScissors } from "react-icons/lu";
 
 export default function HeroSection() {
   return (
@@ -14,15 +15,16 @@ export default function HeroSection() {
             <div className="hs-carousel-slide">
               <div className="h-120 md:h-[calc(100vh-106px)]  flex flex-col bg-[url('/images/heroImage.jpeg')] bg-cover bg-center bg-no-repeat">
                 <div className="mt-auto w-2/3 md:max-w-lg ps-5 pb-5 md:ps-10 md:pb-10">
-                  <span className="block text-white text-2xl font-montserrat font-extrabold">Barber Shop.</span>
+                  <span className="block text-zinc-400 text-2xl font-montserrat font-normal">Barber Shop.</span>
                   <span className="block text-white text-xl md:text-7xl font-new-rocker font-extrabold">
                     The Stylo Cave 
                   </span>
                   <div className="mt-5">
                     <a
-                      className="py-3 px-7 inline-flex items-center font-montserrat gap-x-2 text-xl font-medium rounded-3xl border-red-500 border-[5px] text-black"
+                      className="py-3 px-7 inline-flex items-center font-montserrat gap-x-2 text-xl font-medium rounded-3xl  text-black bg-cuak"
                       href="#"
                     >
+                      <LuScissors />
                       Averigua tu Estilo
                     </a>
                   </div>
