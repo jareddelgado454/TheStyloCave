9fbe6640b9bddd85ef5b2144bc3ada911efa1556
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2025-09-12 09:48:15 -0500

    last changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full flex-1 flex flex-col bg-green-400">
+    <div className="w-full flex-1 flex flex-col bg-zinc-900">
       <HeroSection />
       <h1 className="text-white text-4xl font-montserrat">hijo d epvta</h1>
     </div>
